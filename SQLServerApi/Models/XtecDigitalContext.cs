4e5697bc93559dc61373b420a2bfc8deada4d1ef
Saymon <sam.astua@estudiantec.cr>
2020-11-28 17:48:59 -0600

    [NUEVO] Api Sql Server
    Se crea un Api con la conexión a una base de datos de SQL Server

---
+++ SQLServerApi/Models/XtecDigitalContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SQLServerApi.Models
+{
+    public class XtecDigitalContext: DbContext
+    {
+        public XtecDigitalContext()
+        {
+        }
+
+        public XtecDigitalContext(DbContextOptions<XtecDigitalContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SQLServerApi/Models/XtecDigitalDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SQLServerApi.Models
+{
+    public partial class XtecDigitalDBContext : DbContext
+    {
+        public XtecDigitalDBContext()
+        {
+        }
+
+        public XtecDigitalDBContext(DbContextOptions<XtecDigitalDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
