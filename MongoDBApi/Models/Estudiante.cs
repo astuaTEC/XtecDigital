0ae2452fd96d218820f97e94076395f3adaf38e2
Saymon <sam.astua@estudiantec.cr>
2020-11-28 21:16:48 -0600

    [NUEVO] Api MongoDB
    Se crea una Api para la conexión con una base de datos MongoDB

---
+++ MongoDBApi/Models/Estudiante.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBApi.Models
+{
+    public class Estudiante
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("primerNombre")]
+        public string PrimerNombre { get; set; }
+        [BsonElement("segundoNombre")]
+        public string SegundoNombre { get; set; }
+        [BsonElement("primerApellido")]
+        public string PrimerApellido { get; set; }
+        [BsonElement("segundoApellido")]
+        public string SegundoApellido { get; set; }
+        [BsonElement("carne")]
+        public string Carne { get; set; }
+        [BsonElement("email")]
+        public string Email { get; set; }
+        [BsonElement("password")]
+        public string Password { get; set; }
+    }
+}
