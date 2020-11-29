ca605be33341b589bd41e092f3a0751b64a56282
Saymon <sam.astua@estudiantec.cr>
2020-11-28 21:16:48 -0600

    [NUEVO] Api MongoDB
    Se crea una Api para la conexión con una base de datos MongoDB

---
+++ MongoDBApi/Controllers/EstudianteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDBApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBApi.Controllers
+{
+    [ApiController]
+    public class EstudianteController : ControllerBase
+    {
+        private readonly EstudianteRepo _repo;
+
+        public EstudianteController(EstudianteRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        [Route("api/estudiantes")]
+        public IActionResult Get()
+        {
+            var resultado = _repo.getEstudiantes();
+
+            if (resultado == null)
+                return BadRequest("Algo salió mal");
+            return Ok(resultado);
+        }
+    }
+}
---
+++ MongoDBApi/Repositories/EstudianteRepo.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using MongoDBApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBApi.Repositories
+{
+    public class EstudianteRepo
+    {
+        private readonly IMongoDatabase _database;
+        private IMongoCollection<Estudiante> _estudiantes;
+        public EstudianteRepo(IMongoClient client)
+        {
+            _database = client.GetDatabase("XtecDigitalDB");
+            _estudiantes = _database.GetCollection<Estudiante>("Estudiante");
+        }
+
+        public List<Estudiante> getEstudiantes()
+        {
+            return _estudiantes.Find(x => true).ToList();
+        }
+    }
+}
